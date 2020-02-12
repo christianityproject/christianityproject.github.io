f6437eceb22eae8629debe5144552eec92de804d
christianityproject <59925989+christianityproject@users.noreply.github.com>
2020-02-12 18:53:32 +0300

    Add files via upload

---
+++ js/theory-script.js
@@ -0,0 +1,12 @@
+function darkTheme() {
+  $(".adaptiveColor").css("color", "white");
+  $("body").css("background-color", "#222222");
+}
+function blackTheme() {
+  $(".adaptiveColor").css("color", "white");
+  $("body").css("background-color", "black");
+}
+function whiteTheme() {
+  $(".adaptiveColor").css("color", "black");
+  $("body").css("background-color", "white");
+}
